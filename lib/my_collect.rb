e56661225de64010319414e1e2167861a76eb9e8
Caitlin Majewski <cmajewski@wesleyan.edu>
2021-04-28 13:23:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   counter=0
   collect=[]
   while counter<collection.length do
-    collect<<yield(collection[counter])
+    collect<<(collection[counter])
     end
     collect
 end
